a0910961ec8bab83bb3e5f3d7217d0b4de9f638f
Ridho Hery Winarto <2009041@uwhs.ac.id>
2024-11-07 15:34:43 +0700

    feat: Implement category management system
    - Add CreateCategoryDialog for new category creation
    - Create API route for category operations
    - Define category schema
    - Update CategoryPicker component
    - Add necessary dependencies

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from '@/components/ui/button';
-import { Command, CommandInput } from '@/components/ui/command';
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { TransactionType } from '@/lib/types';
 import { Category } from '@prisma/client';
@@ -15,16 +15,18 @@ interface Props {
 function CategoryPicker({type}: Props) {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState("");
+  const [inputValue, setInputValue] = React.useState("");
 
   const categoriesQuery = useQuery({
     queryKey: ["categories", type],
     queryFn: () =>
       fetch(`/api/categories?type=${type}`).then((res) => res.json()),
+    initialData: [],
   });
 
   const selectedCategory = categoriesQuery.data?.find(
     (category: Category) => category.name === value
-  );
+  ) || null; 
 
   return <Popover open={open} onOpenChange={setOpen}>
     <PopoverTrigger asChild>
@@ -39,8 +41,35 @@ function CategoryPicker({type}: Props) {
       >
         <CommandInput 
           placeholder="Search category..."
+          value={inputValue}
+          onValueChange={setInputValue}
         />
         <CreateCategoryDialog type={type} />
+        <CommandEmpty>
+          <p>
+            Category not found.
+          </p>
+          <p
+            className="text-xs text-muted-foreground"
+          >
+            Tip: Create a new category
+          </p>
+        </CommandEmpty>
+        <CommandGroup>
+          <CommandList>
+            {categoriesQuery.data && categoriesQuery.data.map((category: Category) => (
+              <CommandItem
+                key={category.name}
+                onSelect={currentValue => {
+                  setValue(currentValue);
+                  setOpen((prev) => !prev);
+                }}
+              >
+                <CategoryRow category={category} />
+              </CommandItem>
+            ))}
+          </CommandList>
+        </CommandGroup>
       </Command>
     </PopoverContent>
   </Popover>
--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -1,22 +1,29 @@
 "use client"
 
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { TransactionType } from '@/lib/types'
 import { cn } from '@/lib/utils'
 import { CreateCategorySchema, CreateCategorySchemaType } from '@/schema/categories'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { PlusSquare } from 'lucide-react'
-import React, { useState } from 'react'
+import { CircleOff, Loader2, PlusSquare } from 'lucide-react'
+import React, { useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import Picker from '@emoji-mart/react';
+import data from '@emoji-mart/data';
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { CreateCategory } from '../_actions/categories'
+import { Category } from '@prisma/client'
+import { toast } from 'sonner'
 
 interface Props {
     type: TransactionType
 }
 
-const CreateCategoryDialog = ({ type }: Props) => {
+function CreateCategoryDialog ({ type }: Props) {
     const [open, setOpen] = useState(false);
     const form = useForm<CreateCategorySchemaType>({
         resolver: zodResolver(CreateCategorySchema),
@@ -24,6 +31,43 @@ const CreateCategoryDialog = ({ type }: Props) => {
             type
         }
     });
+
+    const queryClient = useQueryClient();
+
+    const { mutate, isPending } = useMutation({
+        mutationFn: CreateCategory,
+        onSuccess: async (data: Category) => {
+            form.reset({
+                name: "",
+                icon: "",
+                type
+            });
+            toast.success(`Created category ${data.name} created successfully 🎉`, {
+                id: "create-category",
+            });
+
+            await queryClient.invalidateQueries({
+                queryKey: ["categories"],
+            });
+
+            setOpen((prev) => !prev);
+        },
+        onError: () => {
+            toast.error("Something went wrong 😞", {
+                id: "create-category",
+            });
+        },
+    });
+
+    const onSubmit = useCallback((values: CreateCategorySchemaType) => {
+        toast.loading("Creating category...", {
+            id: "create-category",
+        });
+        mutate(values);
+    }, 
+    [mutate]
+    );
+
     return <Dialog open={open} onOpenChange={setOpen}>
                 <DialogTrigger asChild>
                     <Button variant={"ghost"} className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground">
@@ -49,27 +93,95 @@ const CreateCategoryDialog = ({ type }: Props) => {
                     </DialogHeader>
                     {/* TODO */}
                     <Form {...form}>
-                        <form className="space-y-8"></form>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8"></form>
                         <FormField 
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>
-                                    Name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            defaultValue={""} {...field}
-                                        />
-                                    </FormControl>
-                                    <FormDescription>
-                                        Transaction description (optional)
-                                    </FormDescription>
-                            </FormItem>
-                        )}
-                    />
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Name
+                                        </FormLabel>
+                                        {/* TODO: A component is changing an uncontrolled input to be controlled. This is likely caused by the value changing from undefined to a defined value, which should not happen. Decide between using a controlled or uncontrolled input element for the lifetime of the component. More info: https://react.dev/link/controlled-components*/}
+                                        <FormControl>
+                                            <Input
+                                                defaultValue={""} {...field}
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Transaction description (optional)
+                                        </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField 
+                            control={form.control}
+                            name="icon"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Icon
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Popover>
+                                                <PopoverTrigger asChild> 
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className="h-[100px] w-full"
+                                                    >
+                                                        {form.watch("icon") ? (
+                                                            <div className='flex flex-col items-center gap-2'>
+                                                            <span className="text-5xl" role="img">{field.value}</span>
+                                                            <p className="text-xs text-muted-foreground">
+                                                                Click to change
+                                                            </p>
+                                                        </div>
+                                                        ) : (
+                                                            <div className='flex flex-col items-center gap-2'>
+                                                                <CircleOff 
+                                                                    className="h-[48px] w-[48px]"
+                                                                />
+                                                                <p className="text-xs text-muted-foreground">
+                                                                    Click to select
+                                                                </p>
+                                                            </div>
+                                                            
+                                                        )}
+                                                    </Button>
+                                                </PopoverTrigger>
+                                                <PopoverContent
+                                                    className="w-full"
+                                                >
+                                                    <Picker
+                                                        data={data}
+                                                        onEmojiSelect={(emoji: {native: string}) => {
+                                                            field.onChange(emoji.native);
+                                                        }}
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                        </FormControl>
+                                        <FormDescription>
+                                            This is how your category will appear on your dashboard
+                                        </FormDescription>
+                                </FormItem>
+                            )}
+                        />
                     </Form>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button
+                            type="button"
+                            variant={"secondary"}
+                            onClick={() => {
+                                form.reset();
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                    </DialogClose>
+                    <Button onClick={form.handleSubmit(onSubmit)} disabled={isPending}>{!isPending && "Create"} {isPending && <Loader2 className="animate-spin" />}</Button>
+                </DialogFooter>
                 </DialogContent>
             </Dialog>
             }
